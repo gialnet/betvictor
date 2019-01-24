1611c2bb2ad58a88a30f55c7f9331a4e18767db2
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2019-01-24 23:34:01 +0100

    implement persistence layer

--- src/main/java/com/betvictor/text/repositories/dataBetVictor.java
+++ src/main/java/com/betvictor/text/repositories/dataBetVictor.java
@@ -1,6 +1,12 @@
 package com.betvictor.text.repositories;
 
+import com.betvictor.text.data.object.TableHistiry;
+
+import java.util.List;
+
 public interface dataBetVictor {
 
+    Integer SaveRandomText();
+    List<TableHistiry> GetLast10Recors();
 
 }
